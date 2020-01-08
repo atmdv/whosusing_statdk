1d8b0bd0e8c23aee09eb79707762c2dc84b2309f
Andreas Tyge Møller <andreastygemoller@Andreas-MacBook-Pro.local>
2020-01-08 15:38:55 +0100

    code updated for 2018

--- analysis.R
+++ analysis.R
@@ -1,47 +1,33 @@
-pacman::p_load(XML, dplyr, ggplot2)
+pacman::p_load(XML, dplyr, ggplot2, readr)
 
-years_w_data <- c(2010:2017)
+years_w_data <- c(2010:2018)
 
 source("get_data.R")
 data <- lapply(years_w_data, FUN=get_data)
 
 data_all <- do.call("rbind", data)
 
-# Fix organization names
-data_all$Organization <-
-  gsub("258. DAMVAD", "445. DAMVAD Analytics", data_all$Organization)
-data_all$Organization <-
-  gsub("62. Rockwool Fondens Forskningsenhed", "62. Rockwool Fonden", data_all$Organization)
+data_all$Organization <-  recode(data_all$Organization,
+         "258. DAMVAD" = "445. DAMVAD Analytics",
+         "62. Rockwool Fondens Forskningsenhed" = "62. Rockwool Fonden",
+         "475. IMS Health, Real World Evidence Solutions Denmark" = "475. IMS Health Technology Solutions Denmark A/S",
+         "387. Moos-Bjerre Analyse" = "387. Moos-Bjerre & Lange ApS")
 
 # Who's had the most new projects i 2015-2017?
 distinct_projects <- data_all %>% 
   group_by(Project_number) %>% 
   filter(Year==min(Year))
 
-private_companies <- c("445. DAMVAD Analytics",
-                       "53. COWI A/S, Division 1",
-                       "48. Epinion A/S",
-                       "44. PLS RAMBØLL Management A/S",
-                       "329. Incentive",
-                       "162. Teknologisk Institut – Center for Analyse og Erhvervsfremme",
-                       "318. Mploy A/S",
-                       "224. Deloitte Consulting, Deloitte, Statsautoriseret Revisionspartnerselskab",
-                       "480. Højbjerre Brauer Schultz",
-                       "72. Copenhagen Economics",
-                       "387. Moos-Bjerre & Lange ApS",
-                       "243. Oxford Research A/S",
-                       "293. McKinsey & Company",
-                       "411. BDO Consulting",
-                       "481. IRIS Group",
-                       "488. Analyse & Tal",
-                       "461. Marselisborg")
+source("private_companies.R")
 
 most_projects <- distinct_projects %>% 
-  filter(Year>=2015) %>%
+  filter(Year>=2016) %>%
   filter(Organization %in% private_companies) %>% 
   group_by(Organization) %>% 
   summarise(Projects = n()) %>% 
   arrange(desc(Projects))
+
+# write_csv(most_projects, "projects_private_companies.csv")
 
 ggplot(data = most_projects,
        aes(x = reorder(Organization, Projects), y = Projects, fill="#97d8f2")) +
@@ -49,8 +35,8 @@ ggplot(data = most_projects,
   scale_fill_manual(values ="#97d8f2") +
   labs(x="", title = "DAMVAD Analytics started more projects than any other private company in the past three years") +
   coord_flip() +
-  theme_minimal() +
   theme(plot.title = element_text(hjust = 0.9)) +
+  # scale_y_discrete(expand=expand_scale(mult=c(0,0.1))) +
   theme(legend.position = "none")
 
 # New projects in DAMVAD Analytics over the years
